244fc731e1d3a1e20cc861c9854d0a8339ab977d
Erika <semtei@hotmail.com>
2017-05-31 18:57:55 +0100

    Latest version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log('it works');
+$(document).ready(function() {
+	
+
+	if ( $.fn.quote_rotator ) {
+		$('#quotes').quote_rotator({
+			rotation_speed: 10000,
+		});
+	}
+		
+	if ( $.fn.vegas ) {
+
+		$(".site-content").vegas({
+			delay: 10000,
+			cover: true,
+		    slides: [
+		        { src:"media/galaxies.jpg" },
+		        { src:"media/pair.jpg" },
+		        { src:"media/earth.jpg" },
+		        { src:"media/nebula.jpg" },
+		        { src:"media/large_web.jpg" },
+		        { src:"media/.jpg" },
+
+	    	]
+		});
+	}
+});
+
